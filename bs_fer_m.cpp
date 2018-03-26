1b0175f3a23816377ea7c8d70162344752213ac3
Tsuchian <35758215+Tsuchian@users.noreply.github.com>
2018-03-26 12:38:44 +0800

    Update bs_fer_m.cpp

--- bs_fer_m.cpp
+++ bs_fer_m.cpp
@@ -1,3 +1,4 @@
+//Write the Hamiltonian of one band under magnetic field for tight-binding model using Periels subtitution
 #ifndef lapack_complex_double 
 #define lapack_complex_double MKL_Complex16
 #endif
@@ -13,105 +14,77 @@
 using namespace std;
 int main()
 {
-		FILE *out;
-		out=fopen("out.txt","w");
-/*	ofstream out;
-	out.open("out.txt",ios::app);
-	if(!out.is_open())  
-    {  
-        cout<<"Could Not Open File!!!";  
-        exit(EXIT_FAILURE);  
-    }*/
-	double kx,ky;
-	double i1,j1;
-	int i,j;
-	int n , m;
-	int jp1,jm1;
-	double n1,m1;
-	for (n=0;n<101;n++)
+	FILE *out;
+	out = fopen("out.txt", "w");
+	/*	ofstream out;
+		out.open("out.txt",ios::app);
+		if(!out.is_open())
+		{
+			cout<<"Could Not Open File!!!";
+			exit(EXIT_FAILURE);
+		}*/
+	double kx, ky;
+	double i1, j1;
+	int i, j;
+	int n, m;
+	int jp1, jm1;
+	double n1, m1;
+	for (n = 0; n < 101; n++)
 	{
-		for (m=0;m<101;m++)
+		for (m = 0; m < 101; m++)
 		{
-			n1=double(n);
-			m1=double(m);
-		    kx = (-pi)+2.0*pi* n1/100.0;
-		    //kx = -pi+2.0*pi* n1/100.0;
-			ky = (-pi)+2.0*pi* m1/100.0;
-		    //cout << kx << " " << ky <<endl;
-            complex<double> a[N][N]={{0,0}};
+			n1 = double(n);
+			m1 = double(m);
+			kx = (-pi) + 2.0*pi* n1 / 100.0;
+			//kx = -pi+2.0*pi* n1/100.0;
+			ky = (-pi) + 2.0*pi* m1 / 100.0;
+			//cout << kx << " " << ky <<endl;
+			complex<double> a[N][N] = { {0,0} };
 
 
-/*------------------------------------------------- create the matrix------------------------------------------*/        
-		
-		for(i=0;i<N;i++)
-        {
-        	j=i;
-        	jm1=j-1;
-			jp1=j+1;
-        	i1=double(i);
-			a[i][j] ={2.0*cos(ky-mag*2.0*pi*i1/double(N)),0.0};
-			//cout<<2.0*cos(ky-2.0*pi*i1/double(N))<<a[i][j];
-			if (i==N-1)
+			/*------------------------------------------------- create the matrix------------------------------------------*/
+
+			for (i = 0; i < N; i++)
 			{
-				jp1=0;
+				j = i;
+				jm1 = j - 1;
+				jp1 = j + 1;
+				i1 = double(i);
+				a[i][j] = { 2.0*cos(ky - mag*2.0*pi*i1 / double(N)),0.0 };
+				//cout<<2.0*cos(ky-2.0*pi*i1/double(N))<<a[i][j];
+				if (i == N - 1)
+				{
+					jp1 = 0;
+				}
+
+				if (i == 0)
+				{
+					jm1 = N - 1;
+				}
+				a[i][jp1] = { cos(kx),-sin(kx) };
+				a[i][jm1] = { cos(kx),sin(kx) };
 			}
-				
-			if (i==0)
+			/*------------------------------------------------ create the matrix over----------------------------------------*/
+			//next is to diagonalize the matrix a[][]
+			//----------------------------------------------------------------------------------------------------------------
+			//
+
+			char jobz = 'N';
+			char uplo = 'U';
+			double w[N];
+			//lapack_complex_double vl[N];
+			//lapack_complex_double vr[N];
+			lapack_int lda = N;
+			//lapack_int ldvl = N;
+			//lapack_int ldvr = N;
+			LAPACKE_zheev(LAPACK_ROW_MAJOR, jobz, uplo, N, *a, lda, w); //diagonalization
+			fprintf(out, "%f %f ", kx, ky);
+			for (i = 0; i < N; i++)
 			{
-				jm1=N-1;
+				fprintf(out, " %f ", w[i]);
 			}
-        	a[i][jp1] = {cos(kx),-sin(kx)};
-        	a[i][jm1] = {cos(kx),sin(kx)};	
+			fprintf(out, "\n");
 		}
-/*------------------------------------------------ create the matrix over----------------------------------------*/
-//next is to diagonalize the matrix a[][]
-//----------------------------------------------------------------------------------------------------------------
-//
-
-/*for (i=0;i<N;i++)
-{
-		for (j=0;j<N;j++)
-		{
-				cout << a[i][j] << " ";
-		}
-		cout << endl;
-		}
-getchar();*/
-		
-		char jobz='N' ;
-        char uplo='U' ;
-        //float wr[N];
-        //float wi[N];
-
-		double w[N];
-        //lapack_complex_double vl[N];
-        //lapack_complex_double vr[N];
-        lapack_int lda = N;
-        //lapack_int ldvl = N;
-        //lapack_int ldvr = N;
-
-        LAPACKE_zheev(LAPACK_ROW_MAJOR, jobz, uplo, N, *a, lda, w); //diagonalization
-/*for (i=0;i<N;i++)
-{
-		for (j=0;j<N;j++)
-		{
-				cout << real(a[i][j]) << " ";
-		}
-		cout << endl;
-		}
-getchar();*/
-		fprintf(out,"%f %f ",kx,ky);
-		for (i=0;i<N;i++)
-		{
-				//fprintf(out, "%f %f ",kx,ky);
-				fprintf(out, " %f ",w[i]);
-				//cout << w[i] << " ";
-		}
-		fprintf(out, "\n");
-		//cout << endl;
-
 	}
-
-	}
-		fclose(out);
+	fclose(out);
 }
